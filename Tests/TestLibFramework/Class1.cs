110fb90dd01b3cd1b45861a936c537f0735e95d8
susanoo-10011 <cycahosir@gmail.com>
2023-04-17 17:27:19 +0700

    Тестируем использование элементов WPF в библиотеках

---
+++ Tests/TestLibFramework/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TestLibFramework
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter); //функция, которая возвращает либо истину либо ложь.
+                                                           //Если ложь, то команду нельзя выполнить
+
+        public abstract void Execute(object parameter); //то, что должно быть выполнено самой командой
+    }
+}
