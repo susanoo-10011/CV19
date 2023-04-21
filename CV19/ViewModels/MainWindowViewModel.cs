8fc8d2c1515ec98d50a0e1029c313e91bb1647f6
susanoo-10011 <cycahosir@gmail.com>
2023-04-21 21:49:39 +0700

    Добавлена в MaimWindowViewModel команда выхода из приложения

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,12 @@
-﻿using CV19.ViewModels.Base;
+﻿using CV19.Infrastructure.Commands;
+using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace CV19.ViewModels
 {
@@ -31,6 +34,31 @@ namespace CV19.ViewModels
         {
             get => _Status; // просто возвращает значение поля 
             set => Set(ref _Status, value); //вызывает метод Set, который передает ссылку на поле _Status и значение, которое будет установленно(value)
+        }
+
+        #region Команды
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+        private void OnCloseApplicationCommandExecuted(object p) 
+        {
+            Application.Current.Shutdown();        
+        } //метод выполняется когда команда выполняется
+
+        private bool CanCloseApplicationCommandExecute(object p) => true; //команда доступна всегда
+
+        #endregion
+
+        #endregion
+        public MainWindowViewModel()
+        {
+            #region
+
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
+
         }
 
     }
